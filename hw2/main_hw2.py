e32554d99a48f97d38f300cf3932f0fc71a7f5a9
mariia <mariia.yukhym@gmail.com>
2022-10-21 09:48:26 -0400

    did second homework (work with outputs)

---
+++ hw2/main_hw2.py
@@ -0,0 +1,17 @@
+
+# ex.1: print "Hello word"
+print("Hello world")
+
+# ex.2: print 5 asterisk symbols
+print("*" * 5)
+
+# ex.3 first version: create 5*5 square
+square_line = "# " * 5
+print(f"{square_line}\n" * 5)
+
+# ex.3 second version: create 5*5 square
+for line in range(5):
+    print("# " * 5)
+
+
+
